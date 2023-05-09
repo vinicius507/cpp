2257861e60959d3e52413288c6b9c8f163380694
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 19:52:09 -0300

    fix(cpp01/ex00): removes comment from main.cpp

--- cpp-01/ex00/src/main.cpp
+++ cpp-01/ex00/src/main.cpp
@@ -6,13 +6,12 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:52:00 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/09 19:21:15 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:51:58 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "zombies.hpp"
 
-// TODO: make a function to explain when heap allocation is useful
 int main(void) {
   Zombie *marvin;
 
