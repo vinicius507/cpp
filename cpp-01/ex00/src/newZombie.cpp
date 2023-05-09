182163599e9f07ea4c953eeaef177755d2f7f048
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 19:17:02 -0300

    fix(cpp01/ex00): changes Zombie.h header to zombies.h in newZombie

--- cpp-01/ex00/src/newZombie.cpp
+++ cpp-01/ex00/src/newZombie.cpp
@@ -6,10 +6,10 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:16:50 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/09 16:18:37 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:16:24 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include "zombies.hpp"
 
 Zombie *newZombie(std::string name) { return new Zombie(name); }
