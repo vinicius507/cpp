1fb99124556e5dbca7626a6a6b1cc05c0f8568b9
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 19:23:03 -0300

    feat(cpp01/ex00): implements randomChump

--- cpp-01/ex00/src/randomChump.cpp
+++ cpp-01/ex00/src/randomChump.cpp
@@ -1,24 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 15:52:00 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/09 19:21:15 by vgoncalv         ###   ########.fr       */
+/*   Created: 2023/05/09 19:16:08 by vgoncalv          #+#    #+#             */
+/*   Updated: 2023/05/09 19:18:00 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "zombies.hpp"
 
-// TODO: make a function to explain when heap allocation is useful
-int main(void) {
-  Zombie *marvin;
+void randomChump(std::string name) {
+  Zombie me = Zombie(name);
 
-  randomChump("Vini");
-  marvin = newZombie("marvin");
-  marvin->announce();
-  delete marvin;
-  return (0);
+  me.announce();
 }
--- cpp-01/ex00/src/zombies.hpp
+++ cpp-01/ex00/src/zombies.hpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:18:44 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/09 16:19:20 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:22:00 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 #include <iostream>
 
 Zombie *newZombie(std::string name);
+
+void randomChump(std::string name);
 
 #endif // !ZOMBIES_HPP
