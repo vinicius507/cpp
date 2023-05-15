963292e468a0afc552e5212a2185a20b693ef67f
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-15 09:12:18 -0300

    feat(cpp01/ex04): implements search_replace

--- cpp-01/ex04/src/main.cpp
+++ cpp-01/ex04/src/main.cpp
@@ -6,12 +6,14 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 18:19:47 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/12 18:52:19 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/13 17:35:46 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 static inline void argparsing_error(std::string fed, std::string error) {
   std::cerr << "Error: " << error << std::endl
@@ -25,6 +27,23 @@ static inline void argparsing_error(std::string fed, std::string error) {
             << "  REPLACE  The replace string." << std::endl;
 }
 
+static std::string search_replace(std::string line, std::string search,
+                                  std::string replace) {
+  std::stringstream buf;
+  unsigned long int start_pos, search_pos;
+
+  start_pos = 0;
+  search_pos = line.find(search);
+  while (search_pos != std::string::npos) {
+    buf << line.substr(start_pos, search_pos - start_pos) << replace;
+
+    start_pos = search_pos + search.length();
+    search_pos = line.find(search, start_pos);
+  }
+  buf << line.substr(start_pos) << std::endl;
+  return (buf.str());
+}
+
 int main(int argc, char *argv[]) {
   std::string fname;
   std::string search, replace;
@@ -34,15 +53,29 @@ int main(int argc, char *argv[]) {
     return (1);
   }
 
-  fname = std::string(argv[1]);
-  search = std::string(argv[2]);
-  replace = std::string(argv[3]);
-
-  std::fstream file(fname);
+  fname = argv[1];
+  search = argv[2];
+  replace = argv[3];
+  std::ifstream file(fname);
   if (file.is_open() == false) {
-    argparsing_error(argv[0], fname + ": failed to open file.");
+    std::cerr << "Error: failed to open file." << std::endl;
     return (1);
   }
+
+  std::string line;
+  std::stringstream res;
+  while (std::getline(file, line)) {
+    if (file.bad()) {
+      std::cerr << "Error: failed to read file." << std::endl;
+      file.close();
+      exit(1);
+    }
+    res << search_replace(line, search, replace);
+  }
   file.close();
+
+  std::ofstream outfile(fname, std::ofstream::trunc);
+  outfile << res.rdbuf();
+  outfile.close();
   return (0);
 }
