ea962954f1f42d2e7301e92b880d68234bfdf5a0
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-26 15:05:04 -0300

    fix: removes cstdlib exit function because it won't compile

--- cpp-00/ex01/src/prompt.cpp
+++ cpp-00/ex01/src/prompt.cpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:31:38 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/22 15:28:20 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/26 15:04:53 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,5 @@ std::string prompt(std::string prefix) {
 
   std::cout << prefix;
   std::getline(std::cin, ans);
-
-  if (std::cin.eof()) {
-    std::cerr << std::endl << "Error: EOF reached while reading standard input";
-    exit(1);
-  }
-
-  if (std::cin.fail()) {
-    std::cerr << "Error: input failure while reading standard input";
-    exit(1);
-  }
   return ans;
 }
