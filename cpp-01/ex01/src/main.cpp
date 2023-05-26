6d20fec7aded8491c921985b569a63e25727b229
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-26 14:48:46 -0300

    fix: removes std::to_string as it is from C++11 standard

--- cpp-01/ex01/src/main.cpp
+++ cpp-01/ex01/src/main.cpp
@@ -6,21 +6,25 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:30:53 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/09 19:42:30 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/26 14:48:29 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "horde.hpp"
+#include <sstream>
 #include <string>
 
 int main(void) {
   Zombie *horde;
+  std::stringstream ss;
 
   for (int n = 0; n < 10; n++) {
-    horde = zombieHorde(n, "horde-" + std::to_string(n));
+    ss << n;
+    horde = zombieHorde(n, "horde-" + ss.str());
     for (int i = 0; i < n; i++) {
       horde[i].announce();
     }
+    ss.str(std::string());
     delete[] horde;
   }
 }
