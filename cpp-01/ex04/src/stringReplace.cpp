2434fedefd2be64028114f5d768a2075a28e3ba4
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-27 14:06:49 -0300

    fix: fixes casing of parameters

--- cpp-01/ex04/src/stringReplace.cpp
+++ cpp-01/ex04/src/stringReplace.cpp
@@ -6,33 +6,33 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 21:30:42 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/22 15:35:41 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/27 14:05:40 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stringReplace.hpp"
 #include <sstream>
 
-std::string stringReplace(const std::string &str, const std::string &search_str,
-                          const std::string &replace_str, int count) {
+std::string stringReplace(const std::string &str, const std::string &searchStr,
+                          const std::string &replaceStr, int count) {
   size_t start;
   size_t search;
   int substitutions;
   std::stringstream buf;
 
-  if (str.empty() || search_str.empty()) {
+  if (str.empty() || searchStr.empty()) {
     return (str);
   }
   start = 0;
-  search = str.find(search_str);
+  search = str.find(searchStr);
   substitutions = 0;
   while (search != std::string::npos) {
     if (count != ALL_MATCHES && substitutions++ == count) {
       break;
     }
-    buf << str.substr(start, search - start) << replace_str;
-    start = search + search_str.length();
-    search = str.find(search_str, start);
+    buf << str.substr(start, search - start) << replaceStr;
+    start = search + searchStr.length();
+    search = str.find(searchStr, start);
   }
   buf << str.substr(start);
   return (buf.str());
