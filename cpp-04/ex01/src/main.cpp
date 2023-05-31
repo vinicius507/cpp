ce85440ddb7441add671afeaae4c994dfce55ec2
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-31 15:22:43 -0300

    feat: adds the array test from the subject

--- cpp-04/ex01/src/main.cpp
+++ cpp-04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:26:31 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/31 14:47:20 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:21:48 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@
 #include <iomanip>
 #include <iostream>
 
-int main(void) { return (0); }
+#define NUM_ANIMALS 10
+
+static void populateDogs(Animal **animals) {
+  for (int i = 0; i < NUM_ANIMALS / 2; i++) {
+    animals[i] = new Dog();
+  }
+}
+
+static void populateCats(Animal **animals) {
+  for (int i = NUM_ANIMALS / 2; i < NUM_ANIMALS; i++) {
+    animals[i] = new Cat();
+  }
+}
+
 static void basicSubjectTest(void) {
   const Animal *j = new Dog();
   const Animal *i = new Cat();
@@ -25,9 +38,18 @@ static void basicSubjectTest(void) {
   delete i;
 }
 
-int main(void) {
+static void extendedSubjectTest(void) {
   Animal *animals[NUM_ANIMALS] = {NULL};
 
+  populateDogs(animals);
+  populateCats(animals);
+  for (int i = 0; i < NUM_ANIMALS; i++) {
+    delete animals[i];
+  }
+}
+
+int main(void) {
   basicSubjectTest();
+  extendedSubjectTest();
   return (0);
 }
