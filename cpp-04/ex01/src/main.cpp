87009cee200ee681ed116f916349ad6ab2f286c7
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-31 15:33:04 -0300

    feat: adds deep copy tests

--- cpp-04/ex01/src/main.cpp
+++ cpp-04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:26:31 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/31 15:21:48 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:32:56 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,37 @@ static void extendedSubjectTest(void) {
   }
 }
 
+static void printIdeas(Brain *brain) {
+  int i = 0;
+  std::string idea;
+
+  if (brain == NULL)
+    return;
+  while (i < 100 && (idea = brain->getIdea(i)) != "") {
+    std::cout << "Idea #" << i << ": " << idea << "(" << &idea << ")"
+              << std::endl;
+    i++;
+  }
+}
+
+static void deepCopyTest(void) {
+  Cat cat;
+  Cat catCopy;
+  Brain *brain;
+
+  brain = cat.getBrain();
+  brain->newIdea("This is a common idea");
+  catCopy = cat;
+  brain = catCopy.getBrain();
+  brain->newIdea("Unique idea");
+  brain = cat.getBrain();
+  printIdeas(cat.getBrain());
+  printIdeas(catCopy.getBrain());
+}
+
 int main(void) {
   basicSubjectTest();
   extendedSubjectTest();
+  deepCopyTest();
   return (0);
 }
