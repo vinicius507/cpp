bec6b983af216467f257fe7f18c985188150169a
Vinicius Oliveira <vinicius@myriad.codes>
2023-06-01 16:10:21 -0300

    feat: adds arithimetic operators tests

--- cpp-02/ex02/src/main.cpp
+++ cpp-02/ex02/src/main.cpp
@@ -6,14 +6,22 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:33:26 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/25 14:33:38 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/06/01 16:15:57 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iomanip>
 #include <iostream>
 
-int main(void) {
+#define TEST(TEST_CASE)                                                        \
+  do {                                                                         \
+    std::cout << "Test: " << #TEST_CASE << std::endl;                          \
+    TEST_CASE();                                                               \
+    std::cout << std::setw(32) << std::setfill('-') << "" << std::endl;        \
+  } while (0)
+
+static void subjectTests(void) {
   Fixed a;
   Fixed const b(Fixed(5.05f) * Fixed(2));
 
@@ -24,5 +32,33 @@ int main(void) {
   std::cout << a << std::endl;
   std::cout << b << std::endl;
   std::cout << Fixed::max(a, b) << std::endl;
-  return 0;
+}
+
+static void arithimeticTests(void) {
+  Fixed a(3.5f);
+  Fixed b(2);
+  Fixed res;
+
+  res = a + b;
+  std::cout << "[Addition]" << std::endl
+            << "rawBits=" << res.getRawBits() << std::endl
+            << "toFloat=" << res.toFloat() << std::endl;
+  res = a - b;
+  std::cout << "[Subtraction]" << std::endl
+            << "rawBits=" << res.getRawBits() << std::endl
+            << "toFloat=" << res.toFloat() << std::endl;
+  res = a * b;
+  std::cout << "[Multiplication]" << std::endl
+            << "rawBits=" << res.getRawBits() << std::endl
+            << "toFloat=" << res.toFloat() << std::endl;
+  res = a / b;
+  std::cout << "[Division]" << std::endl
+            << "rawBits=" << res.getRawBits() << std::endl
+            << "toFloat=" << res.toFloat() << std::endl;
+}
+
+int main(void) {
+  TEST(subjectTests);
+  TEST(arithimeticTests);
+  return (0);
 }
