32c3ab8cbb19d09083d4192ecb5146100d8b732b
Vinicius Oliveira <vinicius@myriad.codes>
2023-06-06 11:22:47 -0300

    feat: removes flake-utils, adds 42-nix!

--- flake.nix
+++ flake.nix
@@ -1,50 +1,45 @@
 {
-  description = "École 42 development enviroment";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    ft-nix = {
+      url = "github:vinicius507/42-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    ft-nix,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ft-nix.overlays.norminette];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        bear
+        clang-tools_12
+        gnumake
+        llvmPackages_12.libcxxClang
+        norminette
+        valgrind
+      ];
+      shellHook = ''
+        cat << EOF
+        Welcome to the École 42 development environment!
 
-        python-packages = ps: with ps; [
-          (buildPythonPackage rec {
-            pname = "norminette";
-            version = "3.3.51";
-            src = fetchPypi {
-              inherit pname version;
-              sha256 = "7ed0cbd125e2dc0f776ac274d22bdbc9fcea5cfb0e7797e1d6ba8fcf58894771";
-            };
-            doCheck = false;
-          })
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bear
-            clang-tools_12
-            gnumake
-            llvmPackages_12.libcxxClang
-            valgrind
-            (python3.withPackages python-packages)
-          ];
-          shellHook = ''
-            echo "Welcome to 42 development environment!"
-            cat << EOF
-                    :::      ::::::::
-                  :+:      :+:    :+:
-                +:+ +:+         +:+
-              +#+  +:+       +#+
-            +#+#+#+#+#+   +#+
-                #+#    #+#
-                ###   ########.fr
-            EOF
-          '';
-        };
-      });
+                :::      ::::::::
+              :+:      :+:    :+:
+            +:+ +:+         +:+
+          +#+  +:+       +#+
+        +#+#+#+#+#+   +#+
+            #+#    #+#
+            ###   ########.fr
+        EOF
+      '';
+    };
+  };
 }
