9cbd8874273fb1ac729c78b18308e4a16ad611a6
Vinicius Oliveira <vinicius@myriad.codes>
2023-06-06 17:03:17 -0300

    fix: ICharacter now is able to create vtable

--- cpp-04/ex03/src/ICharacter.hpp
+++ cpp-04/ex03/src/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 15:55:33 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/06/06 15:29:43 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/06/06 16:55:00 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class AMateria;
 
 class ICharacter {
 public:
-  virtual ~ICharacter();
+  virtual ~ICharacter(){};
 
   virtual const std::string &getName(void) const = 0;
 
@@ -27,7 +27,7 @@ public:
 
   virtual void unequip(int idx) = 0;
 
-  virtual void use(int idx, ICharacter &target);
+  virtual void use(int idx, ICharacter &target) = 0;
 };
 
 #endif // !ICHARACTER_HPP
