56bd3a13891c8dbd3542b481b31e4c20b44b053a
Vinicius Oliveira <vinicius@myriad.codes>
2023-06-16 16:44:54 -0300

    chore: adds documentation to operator overloads for the Data struct

--- cpp-06/ex01/src/Data.hpp
+++ cpp-06/ex01/src/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 15:39:31 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/06/16 16:19:37 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:44:34 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,21 @@ struct Data {
   int age;
 };
 
+/**
+ * @brief Insertion operator for the Data struct.
+ *
+ * @param out The output stream.
+ * @param data The data object to be inserted.
+ */
 std::ostream &operator<<(std::ostream &out, Data &data);
 
+/**
+ * @brief Equality operator for the Data struct.
+ *
+ * @param lhs The left hand side of the equality operator.
+ * @param rhs  The right hand side of the equality operator.
+ * @return `true` if the records names and ages are equal, otherwise `false`.
+ */
 bool operator==(Data &lhs, Data &rhs);
 
 #endif // !DATA_HPP
