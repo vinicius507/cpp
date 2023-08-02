a072f0c9de75d3e0918911b24736c770c048f36d
Vinicius Oliveira <vinicius@myriad.codes>
2023-08-02 17:32:48 -0300

    fix: removes duplicated test in main

--- cpp-07/ex02/src/main.cpp
+++ cpp-07/ex02/src/main.cpp
@@ -108,7 +108,6 @@ int main(void) {
   TEST(testSmallDefaultArray());
   TEST(testLargeDefaultArray());
   TEST(testIndexOutOfBounds());
-  TEST(testIndexOutOfBounds());
   TEST(testCopyConstructor());
   TEST(testAssignmentOperator());
   return (0);
